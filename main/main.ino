0e320003b454a628a4aef203b9c41fe372c5c4b9
Sam Ansari <elucidation@gmail.com>
2013-10-01 13:32:09 -0400

    Added usonic section&library

--- main/main.ino
+++ main/main.ino
@@ -9,18 +9,21 @@ Sameer Ansari
 
 */
 
+#include <ultrasonic.h>
+
 //////////////////////////////////////////////////////////////////////////////
 /// 
 /// DEFINES
 ///
 /// constants should be defined here, such as Pin numbers or math constants
+/// Note: Keep pin order from least-most.
 ///
 /// ex: #define PI 3.14159
 ///
-///	#define POT_PIN 9
-///
 //////////////////////////////////////////////////////////////////////////////
 
+#define ULTRASONIC_TRIG 2
+#define ULTRASONIC_ECHO 3
 #define POT_PIN 9
 
 //////////////////////////////////////////////////////////////////////////////
@@ -29,11 +32,14 @@ Sameer Ansari
 ///
 /// Create global variables here.  Try not to use too many, it's not a great
 /// coding practice
+/// Note: Keep name order from least-most.
 ///
 /// All global variables should be prefaced with "g_"   - ex: g_motorState
 ///
 //////////////////////////////////////////////////////////////////////////////
 
+// Ultrasonic sensor that returns readings in millimeters.
+Ultrasonic g_UsonicSensor(ULTRASONIC_TRIG, ULTRASONIC_ECHO);
 
 /*
 **
@@ -46,8 +52,27 @@ int g_motorState = 0;
 
 
 void setup() {
+  // Initialize serial communication.
+  Serial.begin(9600);
+
+  //// Pins
+  Serial.println("Initializing pins...");
 	pinMode(POT_PIN, INPUT);
+
+  
+  //// Sensors
+  Serial.println("Initializing sensors...");
+  
+  // Initialize Ultrasonic sensor
+  g_UsonicSensor.init();
+
+
+  Serial.println("Initialized.");
 }
 
 void loop() {
+
+  //// Sam
+  // Ultrasonic sensor + motor
+  unsigned long usonic_dist_mm = g_UsonicSensor.getReading();
 }
