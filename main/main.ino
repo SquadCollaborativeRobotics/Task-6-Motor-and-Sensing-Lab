66852fd1ab2c31383c26efcb73648d93989dec5c
Shawn <shawn@shawn-Lenovo-IdeaPad-Y400.(none)>
2013-09-30 21:08:15 -0400

    Initial commit

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,52 @@
 /*
-TODO : Info
- */
- 
-void setup() {  
+Task 2: Motor and Sensor Lab
+Team E
+
+Aaron Nye
+Shawn Hanna
+Alex Sher
+Sameer Ansari
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+/// 
+/// DEFINES
+///
+/// constants should be defined here, such as Pin numbers or math constants
+///
+/// ex: #define PI 3.14159
+///
+///	#define POT_PIN 9
+///
+//////////////////////////////////////////////////////////////////////////////
+
+#define POT_PIN 9
+
+//////////////////////////////////////////////////////////////////////////////
+/// 
+/// Global Variables
+///
+/// Create global variables here.  Try not to use too many, it's not a great
+/// coding practice
+///
+/// All global variables should be prefaced with "g_"   - ex: g_motorState
+///
+//////////////////////////////////////////////////////////////////////////////
+
+
+/*
+**
+** g_motorState specifies which motor mode we are running in, which picks
+** which motor/sensor combination to operate (changes via button press)
+**
+*/
+
+int g_motorState = 0;
+
+
+void setup() {
+	pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
